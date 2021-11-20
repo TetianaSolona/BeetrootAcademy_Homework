a1a54e3bb76ebf522b2cfc6635655c1285e04294
TetianaSolona <94628755+TetianaSolona@users.noreply.github.com>
2021-11-20 18:57:12 +0200

    Hometask_1

---
+++ lesson1.py
@@ -0,0 +1,41 @@
+if __name__ == '__main__':
+
+    # task2
+    print('Hello', 'world!', sep=', ')
+    print('Hello,', 'world', end='! \n')
+
+    # task3
+    SYMBOL = '#'
+    print(SYMBOL * 9)
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+    print(SYMBOL * 9)
+    print()
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+    print(SYMBOL * 9)
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+    print(SYMBOL, '\t\t', SYMBOL, sep='')
+
+    # task 1 from lesson 3
+    # The greeting program
+    name = 'Tania'
+    day = 'Saturday'
+    print(f'Good day {name}! {day} is a perfect day to learn some python.')
+
+    # task 2 from lesson 3
+    first_name = 'Tania'
+    second_name = 'Solona'
+    print('Hello', first_name + ' ' + second_name)
+
+    # task 3 from lesson 3
+    x = 5
+    y = 3
+    print('Multiplication:', x * y)
+    print('Division:', x / y)
+    print('Addition:', x + y)
+    print('Subtraction:', x - y)
+    print('Exponent:', x ** y)
+    print('Residue:', x % y)
+    print('Whole division:', x // y)
