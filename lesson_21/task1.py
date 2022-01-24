ecd8e27e27a89ab3b3d69c7b05102850b09fd4e1
TetianaSolona <94628755+TetianaSolona@users.noreply.github.com>
2022-01-24 21:56:43 +0200

    Context manager

--- lesson_21/task1.py
+++ lesson_21/task1.py
@@ -1,13 +1,17 @@
+import os.path
 import time
+import unittest
 
 
+# task 1
 class File:
+
     def __init__(self, file_path):
         self.file_path = file_path
 
     def __enter__(self):
         self.start_time = time.time()
-        self.file_obj = open(self.file_path, mode="w")
+        self.file_obj = open(self.file_path, mode='w')
         return self.file_obj
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -16,6 +20,27 @@ class File:
         print(time.time() - self.start_time)
 
 
+# task 2
+class TestFile(unittest.TestCase):
+    def test_open(self):
+        with File('hello.txt') as file:
+            self.assertFalse(file.close())
+
+    def test_close(self):
+        with File('hello.txt') as file:
+            self.assertTrue(os.path.exists('hello.txt'))
+
+    def test_time(self):
+        with File('hello.txt') as file:
+            start_time = time.time()
+            file.close()
+            finish_time = time.time()
+        time_work = finish_time - start_time
+        self.assertEqual(time_work, finish_time - start_time)
+
+
 if __name__ == '__main__':
-    with File("hello.txt") as file:
-        file.write("Hello, World!")
+    with File('hello.txt') as file:
+        file.write('Hello, World!')
+
+    unittest.main()
